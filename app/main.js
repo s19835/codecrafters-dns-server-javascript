d69fb371c238d3ccfcf280fab6b6a8ea1e7c1f05
s19835 <s19835@sci.pdn.ac.lk>
2024-07-03 17:24:26 +0530

    Write header section #TZ1

--- app/main.js
+++ app/main.js
@@ -5,7 +5,8 @@ udpSocket.bind(2053, "127.0.0.1");
 
 udpSocket.on("message", (buf, rinfo) => {
   try {
-    const response = Buffer.from("");
+    const message = new DNSmessage(); //create new class instance for message
+    const response = Buffer.from(message.createDNSheader());
     udpSocket.send(response, rinfo.port, rinfo.address);
   } catch (e) {
     console.log(`Error receiving data: ${e}`);
@@ -20,3 +21,20 @@ udpSocket.on("listening", () => {
   const address = udpSocket.address();
   console.log(`Server listening ${address.address}:${address.port}`);
 });
+
+class DNSmessage {
+  constructor() {
+    this.header = this.createDNSheader();
+  }
+
+  createDNSheader() {
+    const header = Buffer.alloc(12); //DNS header 12 byte long
+    header.writeUInt16BE(1234, 0); //Transaction ID
+    header.writeUInt16BE(0x8000, 2); //Flags - QR, OPCODE, AA, TC, RD, RA, Z, and RCODE
+    header.writeUInt16BE(1, 4); // QDCOUNT: 1 (one question)
+    header.writeUInt16BE(0, 6); // ANCOUNT: 0
+    header.writeUInt16BE(0, 8); // NSCOUNT: 0
+    header.writeUInt16BE(0, 10); // ARCOUNT: 0
+    return header;
+  }
+}
