a5855e733528bbb27df5c968a0d9094bdc4cedfb
s19835 <s19835@sci.pdn.ac.lk>
2024-07-04 09:56:17 +0530

    Parse header section #UC8

--- app/main.js
+++ app/main.js
@@ -5,15 +5,19 @@ udpSocket.bind(2053, "127.0.0.1");
 
 udpSocket.on("message", (buf, rinfo) => {
   try {
-    const message = new DNSmessage(); //create new class instance for message
+    const message = new DNSmessage(buf); //create new class instance for message
+    
     const header = message.createDNSheader();
     const question = message.createDNSquestion();
     const answer = message.createDNSanswer();
+    
     const response = Buffer.concat([ 
       header, 
       question, 
       answer 
     ]);
+    
+    
     udpSocket.send(response, rinfo.port, rinfo.address);
   } catch (e) {
     console.log(`Error receiving data: ${e}`);
@@ -30,16 +34,28 @@ udpSocket.on("listening", () => {
 });
 
 class DNSmessage {
-  constructor() {
-    this.header = this.createDNSheader();
-    this.question = this.createDNSquestion();
-    this.answer = this.createDNSanswer();
+  constructor(buffer) {
+
+    if (buffer instanceof Buffer) this.buffer = buffer;
+    this.reciverMessage = this.parseHeader()["message"];
+    this.reciverFlag = this.parseHeader()["flag"];
   }
 
   createDNSheader() {
     const header = Buffer.alloc(12); //DNS header 12 byte long
-    header.writeUInt16BE(1234, 0); //Transaction ID
-    header.writeUInt16BE(0x8000, 2); //Flags - QR, OPCODE, AA, TC, RD, RA, Z, and RCODE
+    header.writeUInt16BE(this.reciverMessage["ID"], 0); //Transaction ID
+
+    //assigning new values to flags
+    this.reciverFlag["QR"] = 1;
+    this.reciverFlag["AA"] = 0;
+    this.reciverFlag["RA"] = 0;
+    this.reciverFlag["Z"] = 0;
+    if (this.reciverFlag["OPCODE"] === 0) this.reciverFlag["RCODE"] = 0;
+    else this.reciverFlag["RCODE"] = 4;
+
+    const senderFlag = this.reciverFlag["QR"] << 15 | this.reciverFlag["OPCODE"] << 11 | this.reciverFlag["AA"] << 10 | this.reciverFlag["TC"] << 9 | this.reciverFlag["RD"] << 8 | this.reciverFlag["RA"] << 7 | this.reciverFlag["Z"] << 4 | this.reciverFlag["RCODE"];
+
+    header.writeUInt16BE(senderFlag, 2); //Flags - QR, OPCODE, AA, TC, RD, RA, Z, and RCODE
     header.writeUInt16BE(1, 4); // QDCOUNT: 1 (one question)
     header.writeUInt16BE(1, 6); // ANCOUNT: 1 (one answer)
     header.writeUInt16BE(0, 8); // NSCOUNT: 0
@@ -87,6 +103,31 @@ class DNSmessage {
     return answer;
   }
 
+  parseHeader() {
+    const flags = this.buffer.readUInt16BE(2);
 
+    const reciverMessage = {
+      ID : this.buffer.readUInt16BE(0),
+      QDCOUNT : this.buffer.readUInt16BE(4),
+      ANCOUNT : this.buffer.readUInt16BE(6),
+      NSCOUNT : this.buffer.readUInt16BE(8),
+      ARCOUNT : this.buffer.readUInt16BE(10),
+    }
+
+    //decomposing flags use:bitwise operations
+    const reciverFlag = {
+      QR : (flags & 0x8000) >> 15,
+      OPCODE : (flags & 0x7800) >> 11,
+      AA : (flags & 0x0400) >> 10,
+      TC : (flags & 0x0200) >> 9,
+      RD : (flags & 0x0100) >> 8,
+      RA : (flags & 0x0080) >> 7,
+      Z : (flags & 0x0070) >> 4,
+      RCODE : flags & 0x000F
+    }
+
+    return { message:reciverMessage, flag:reciverFlag };
+
+  }
 
 }
